59419d72a8976a47b8ec3be8757c0c5c353cbde0
visor517 <visor517@yandex.ru>
2022-02-06 22:56:15 +0300

    added player move

--- run.py
+++ run.py
@@ -13,6 +13,29 @@ def main():
     ui.setupUi(MainWindow)
     MainWindow.show()
 
+    cur_game = Game('X', 'Y')
+
+    def gameOver(message):
+        msg = QMessageBox()
+        msg.setText(message)
+        msg.setWindowTitle("THE END")
+        msg.exec_()
+
+    # ход игрока
+    def makeMove():
+        field = app.sender()
+        field.setText(cur_game.player_char)
+        field.setEnabled(False)
+        move = tuple([int(i) for i in field.objectName().split('_')[1:]])
+        cur_game.makeMove(move, cur_game.player_char)
+        if not cur_game.checkLose(move, cur_game.player_char):
+            gameOver("Game over!")
+
+
+    for item in list(ui.buttonGroup.buttons()):
+        item.clicked.connect(makeMove)
+        
+
     sys.exit(app.exec_())
 
 if __name__ == '__main__':
